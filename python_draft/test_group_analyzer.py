e47f802f0e5101d5041762f4f76526c7b0ab81e6
Jose Blanca <jblanca@upv.es>
2026-03-06 13:12:03 +0100

    implement sample_origins

--- python_draft/test_group_analyzer.py
+++ python_draft/test_group_analyzer.py
@@ -3,6 +3,7 @@ from variant_group_analyzer import (
     VariantIteratorInfo,
     variant_from_dict,
     variant_generator,
+    merge_variant_group,
 )
 
 
@@ -16,7 +17,7 @@ def create_variant_group_iterator(variant_dicts: list[dict], samples: list[str])
     return iter(variant_group_iterable)
 
 
-def test():
+def test_boiler_plate_for_var_group_creation():
     variant_sample1 = {
         "chrom": 1,
         "pos": 10,
@@ -34,14 +35,33 @@ def test():
     var_group_iter = create_variant_group_iterator(
         [variant_sample1, variant_sample2], ["sample1", "sample2"]
     )
-    var_group = list(var_group_iter)[0]
+    var_group, samples = list(var_group_iter)[0]
     assert var_group.start == 10
     assert var_group.end == 10
+    assert samples == [["sample1"], ["sample2"]]
 
 
-def main():
-    test()
-
-
-if __name__ == "__main__":
-    main()
+def test_simple_merge():
+    variant_sample1 = {
+        "chrom": 1,
+        "pos": 10,
+        "alleles": ["A", "T"],
+        "genotypes": [[1, 1]],
+        "phase": [False],
+    }
+    variant_sample2 = {
+        "chrom": 1,
+        "pos": 10,
+        "alleles": ["A", "T"],
+        "genotypes": [[0, 1]],
+        "phase": [False],
+    }
+    var_group_iter = create_variant_group_iterator(
+        [variant_sample1, variant_sample2], ["sample1", "sample2"]
+    )
+    var_group, samples = list(var_group_iter)[0]
+    merge_variant_group(
+        var_group,
+        samples_have_one_var_per_position=True,
+        samples_for_variants=samples,
+    )
--- python_draft/variant_group_analyzer.py
+++ python_draft/variant_group_analyzer.py
@@ -1,5 +1,22 @@
 from dataclasses import dataclass
 from typing import Iterator, Iterable
+from typing import Generic, TypeVar, Union
+
+T = TypeVar("T")
+E = TypeVar("E")
+
+
+@dataclass(frozen=True)
+class Ok(Generic[T]):
+    value: T
+
+
+@dataclass(frozen=True)
+class Err(Generic[E]):
+    error: E
+
+
+Result = Union[Ok[T], Err[E]]
 
 
 @dataclass
@@ -34,7 +51,7 @@ class OverlappingVariantGroup:
     start: int
     end: int
     variants: list[Variant]
-    source_var_iter_idxs: list[int]
+    sample_origins: list[tuple[int, int]]  # list[(iter_idx, sample_idx_in_vcf)]
 
     def __str__(self):
         return f"{self.chrom}:{self.start}-{self.end} ({len(self.variants)})"
@@ -54,8 +71,20 @@ class VariantGroupIterator:
         self.variant_iterators = variant_iterators
         self.iter_infos = iter_infos
 
-    def __next__(self) -> OverlappingVariantGroup:
-        variants = [next(var_iter) for var_iter in self.variant_iterators]
+    def __next__(self) -> tuple[OverlappingVariantGroup, list[list[str]]]:
+        variants: list[Variant] = []
+        sample_origins: list[tuple[int, int]] = []
+        for iter_idx, (var_iter, iter_info) in enumerate(
+            zip(self.variant_iterators, self.iter_infos)
+        ):
+            for sample_idx_in_iter, sample in enumerate(iter_info.samples):
+                sample_origins.append((iter_idx, sample_idx_in_iter))
+                variants.append(next(var_iter))
+
+        variant_samples: list[list[str]] = [
+            iter_info.samples for iter_info in self.iter_infos
+        ]
+
         start = min([variant.pos for variant in variants])
         end = max([len(variant.alleles[0]) + variant.pos - 1 for variant in variants])
         var_group = OverlappingVariantGroup(
@@ -63,9 +92,9 @@ class VariantGroupIterator:
             chrom=variants[0].chrom,
             start=start,
             end=end,
-            source_var_iter_idxs=list(range(len(self.variant_iterators))),
+            sample_origins=sample_origins,
         )
-        return var_group
+        return var_group, variant_samples
 
     def __iter__(self):
         return self
@@ -81,5 +110,55 @@ class VariantGroupIterable:
         self.variant_iterators = variant_iterators
         self.iter_infos = iter_infos
 
-    def __iter__(self) -> Iterable[OverlappingVariantGroup]:
+    def __iter__(self) -> Iterator[tuple[OverlappingVariantGroup, list[list[str]]]]:
         return VariantGroupIterator(self.variant_iterators, self.iter_infos)
+
+
+def create_variant_for_region(
+    variant_group: OverlappingVariantGroup,
+    region: tuple[int, int],
+    samples_for_variants: list[list[str]],
+):
+
+    samples = []
+    sample_origins = []
+    for iter_idx, samples_in_var in enumerate(samples_for_variants):
+        for sample_idx_in_iter, sample in enumerate(samples_in_var):
+            samples.append(sample)
+            sample_origins.append((iter_idx, sample_idx_in_iter))
+
+    samples = [
+        sample for samples_in_var in samples_for_variants for sample in samples_in_var
+    ]
+    print(samples)
+
+    for samples_in_var, variant in zip(samples_for_variants, variant_group.variants):
+        print(variant)
+        print(samples_in_var)
+
+
+def merge_variant_group(
+    variant_group: OverlappingVariantGroup,
+    samples_for_variants: list[list[str]],
+    samples_have_one_var_per_position: bool,
+) -> Result[list[Variant], str]:
+
+    if not samples_have_one_var_per_position:
+        return Err(
+            "NotImplemented, only implemented for VCFs with one line per position"
+        )
+
+    # in some cases all the region covered by the group might be solved in a single variant
+    # the problem happens when we have to heterozygotes and the phase between them is unknown
+    # right now we a going to set the genotype for these heterozyous samples as missing and, thus
+    # we are going to workaround the problem
+    # so the region covered by the resulting variant will the same as the region covered by the
+    # group and only one variant will be constructed for a given group.
+    variant_regions: list[tuple[int, int]] = [(variant_group.start, variant_group.end)]
+
+    variants: list[Variant] = []
+    for region in variant_regions:
+        variant = create_variant_for_region(variant_group, region, samples_for_variants)
+        variants.append(variant)
+
+    return Ok(variants)
