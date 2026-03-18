b94efe0e87239feec75138459279a8bfbbdc4aa0
Jose Blanca <jblanca@upv.es>
2026-03-18 12:01:59 +0100

    make genotypes in test alternative

--- tests/genotype_merging_test.rs
+++ tests/genotype_merging_test.rs
@@ -17,7 +17,7 @@ fn make_iter(vcf_data: &str) -> VariantIterator<BufReader<BufReader<&[u8]>>> {
 
 const VCF1: &str = "##\n\
     #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tNA00001\n\
-    20\t1\t.\tG\tA\t20\tPASS\t.\tGT\t0|0\n\
+    20\t1\t.\tG\tA\t20\tPASS\t.\tGT\t1|1\n\
     20\t2\t.\tT\tA\t20\tPASS\t.\tGT\t.|0\n\
     20\t3\t.\tA\tG\t20\tPASS\t.\tGT\t.|0\n\
     20\t4\t.\tT\t<NON_REF>\t20\tPASS\t.\tGT\t0|0\n\
@@ -26,7 +26,7 @@ const VCF1: &str = "##\n\
 
 const VCF2: &str = "##\n\
     #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tNA00002\n\
-    20\t1\t.\tG\tA\t20\tPASS\t.\tGT\t0|0\n\
+    20\t1\t.\tG\tA\t20\tPASS\t.\tGT\t1|1\n\
     20\t2\t.\tT\tA\t20\tPASS\t.\tGT\t.|0\n\
     20\t3\t.\tA\tG\t20\tPASS\t.\tGT\t.|0\n\
     20\t4\t.\tT\t<NON_REF>\t20\tPASS\t.\tGT\t0|0\n\
@@ -35,19 +35,19 @@ const VCF2: &str = "##\n\
 
 const VCF3: &str = "##\n\
     #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tNA00003\n\
-    20\t1\t.\tGTATGG\tG\t20\tPASS\t.\tGT\t0|0";
+    20\t1\t.\tGTATGG\tG\t20\tPASS\t.\tGT\t1|1";
 
 const VCF4: &str = "##\n\
     #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tNA00004\n\
-    1\t4\t.\tGATCGAT\tG\t20\tPASS\t.\tGT\t0|0\n\
-    20\t4\t.\tTGGAGCAT\tT\t20\tPASS\t.\tGT\t0|0\n\
-    21\t4\t.\tGATCGAT\tG\t20\tPASS\t.\tGT\t0|0";
+    1\t4\t.\tGATCGAT\tG\t20\tPASS\t.\tGT\t1|1\n\
+    20\t4\t.\tTGGAGCAT\tT\t20\tPASS\t.\tGT\t1|1\n\
+    21\t4\t.\tGATCGAT\tG\t20\tPASS\t.\tGT\t1|1";
 
 const VCF5: &str = "##\n\
     #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tNA00005\n\
-    20\t3\t.\tA\tT\t20\tPASS\t.\tGT\t0|0\n\
-    20\t8\t.\tG\tA\t20\tPASS\t.\tGT\t0|0\n\
-    20\t12\t.\tG\tA\t20\tPASS\t.\tGT\t0|0";
+    20\t3\t.\tA\tT\t20\tPASS\t.\tGT\t1|1\n\
+    20\t8\t.\tG\tA\t20\tPASS\t.\tGT\t1|1\n\
+    20\t12\t.\tG\tA\t20\tPASS\t.\tGT\t1|1";
 
 #[test]
 fn test_analyze_produces_one_variant_per_group() {
